56a62bfa96a033df93d57f6723e2e3dfda1dcb44
Jijie Chen <jijie.chen@someplace.com>
2021-03-14 20:46:12 +0800

    为 CloudStorage 添加了功能入口类；在 Dockerfile 中引用更新的版本

---
+++ SharpCR.Features.CloudStorage/CloudStorageFeature.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace SharpCR.Features.CloudStorage
+{
+    public class CloudStorageFeature : IFeature
+    {
+        public void ConfigureServices(IServiceCollection services, StartupContext context)
+        {
+            var configuration = context.Configuration.GetSection("Features:CloudStorage")?.Get<CloudStorageConfiguration>() ?? new CloudStorageConfiguration();
+            services.AddSingleton(Options.Create(configuration));
+            services.AddSingleton<CloudBlobStorage>();
+
+        }
+
+        public void ConfigureWebAppPipeline(IApplicationBuilder app, IServiceProvider appServices, StartupContext context)
+        {
+
+        }
+    }
+}
--- publish.ps1
+++ publish.ps1
@@ -2,7 +2,8 @@ cd SharpCR.Features.CloudStorage
 dotnet build -c Release -r linux-x64
 
 cd ..
-cd SharpCR.Features.CloudStorage
+cd SharpCR.Features.ImageSyncIntegration
 dotnet build -c Release -r linux-x64
 
-docker build -t "sharpcr-registry-internal:1.0.0" .
+cd ..
+docker build -t "sharpcr-registry-internal:1.0.1" .

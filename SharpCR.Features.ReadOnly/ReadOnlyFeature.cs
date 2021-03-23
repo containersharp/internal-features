da03ebd748c26f03db2da4e1df8e13173853a8b4
Jijie Chen <jijie.chen@someplace.com>
2021-03-23 22:11:04 +0800

    添加了一个用于支持 只读 模式的功能

---
+++ SharpCR.Features.ReadOnly/ReadOnlyFeature.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SharpCR.Features.ReadOnly
+{
+    public class ReadOnlyFeature : IFeature
+    {
+        public void ConfigureServices(IServiceCollection services, StartupContext context)
+        {
+            services.AddScoped<ReadOnlyMiddleware>();
+        }
+
+        public void ConfigureWebAppPipeline(IApplicationBuilder app, IServiceProvider appServices, StartupContext context)
+        {
+            app.UseMiddleware<ReadOnlyMiddleware>();
+        }
+    }
+}
---
+++ SharpCR.Features.ReadOnly/ReadOnlyMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace SharpCR.Features.ReadOnly
+{
+    public class ReadOnlyMiddleware: IMiddleware
+    {
+        private static readonly HttpMethod[] UpdatingRequestMethods = new HttpMethod[]
+        {
+            HttpMethod.Put,
+            HttpMethod.Post,
+            HttpMethod.Patch,
+            HttpMethod.Delete
+        };
+        
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            if (Enum.TryParse(typeof(HttpMethod), context.Request.Method, out var httpMethodObj)
+            && Array.IndexOf(UpdatingRequestMethods, (HttpMethod) httpMethodObj) > -1)
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.CompleteAsync();
+                return;
+            }
+            
+            await next(context);
+        }
+    }
+}

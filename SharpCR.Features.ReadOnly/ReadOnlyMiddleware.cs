23ab1cfb86dc23e5616dc45dd3f0e12d75c9cf5c
Jijie Chen <jijie.chen@someplace.com>
2021-04-03 17:42:48 +0800

    在 ReadOnly 中添加日志

--- SharpCR.Features.ReadOnly/ReadOnlyMiddleware.cs
+++ SharpCR.Features.ReadOnly/ReadOnlyMiddleware.cs
@@ -2,12 +2,20 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace SharpCR.Features.ReadOnly
 {
     public class ReadOnlyMiddleware: IMiddleware
     {
-        private static readonly HttpMethod[] UpdatingRequestMethods = new HttpMethod[]
+        private readonly ILogger<ReadOnlyMiddleware> _logger;
+
+        public ReadOnlyMiddleware(ILogger<ReadOnlyMiddleware> logger)
+        {
+            _logger = logger;
+        }
+        
+        private static readonly HttpMethod[] UpdatingRequestMethods =
         {
             HttpMethod.Put,
             HttpMethod.Post,
@@ -20,6 +28,8 @@ namespace SharpCR.Features.ReadOnly
             var method = new HttpMethod(context.Request.Method);
             if (Array.IndexOf(UpdatingRequestMethods, method) > -1)
             {
+                var uriPath = context.Request.Path.Value;
+                _logger.LogInformation("Request rejected because this site is readonly {@req}", new {httpMethod = method.Method, uriPath });
                 context.Response.StatusCode = 404;
                 await context.Response.CompleteAsync();
                 return;
